2967449fea8a538e1f85bdac267b2528c79fffce
vineethreddybh <147896177+vineethreddybh@users.noreply.github.com>
2023-10-14 00:12:00 -0400

    portfolio changes

---
+++ MyWebsite/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer", "Full Stack Developer", "Cybersecurity Analyst"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
